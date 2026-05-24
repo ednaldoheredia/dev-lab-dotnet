a7cad6ea8dfbc6fb1bbea890f925bf143da2a645
Ednaldo Oliveira <ednaldo.programmer@gmail.com>
2026-05-24 16:12:53 -0300

    inclusão de campo

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace Poc_01
         }
 
         //comentário ....ddddd//correddd
-        public static IHostBuilder CreateHostBuilder(string[] argss) =>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
