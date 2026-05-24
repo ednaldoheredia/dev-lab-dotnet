ada91df046804c933e04ab3948f3ac3ef1757759
Ednaldo Oliveira <ednaldo.programmer@gmail.com>
2026-05-24 20:04:22 -0300

    Incl

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Poc_01
             CreateHostBuilder(args).Build().Run();
         }
 
-        //comentário ....ddddd//correddd..test
+        //comentário ....ddddd//correddd..test,,,,,
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
