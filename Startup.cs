d78644da1a80dd9cf33cc99213348f0c38f0076e
Ednaldo Oliveira <ednaldo.programmer@gmail.com>
2026-06-04 15:23:59 -0300

    Ajuste Yaml para não imagem no processo de Build

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,7 @@ namespace Poc_01
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
